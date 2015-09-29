b619562678beab25d18cf5ca789f6c745588083d
gsingh <gurmeet.aurora@gmail.com>
2015-09-29 08:06:12 +0530

    changed startRF.sh

--- startRF.sh
+++ startRF.sh
@@ -2,14 +2,14 @@
 # make commands for rf24 libraries and 
 # to compile and run hareceiver in raspi.
 
-sudo make install --directory=/home/rf24libs/RF24/ && \
+ make install --directory=/home/rf24libs/RF24/ && \
 	make install --directory=/home/rf24libs/RF24Network/ && \ 
 	make install --directory=/home/rf24libs/RF24Mesh/  && \
 	make install --directory=/home/rf24libs/RF24Gateway/
 
-sudo make -B --directory=/home/rf24libs/RF24Gateway/examples/ncurses/ && \
+ make -B --directory=/home/rf24libs/RF24Gateway/examples/ncurses/ && \
 	make -B --directory=/home/rf24libs/
 
 cd /home/rf24libs
 sudo chmod +x 	/home/rf24libs/hareceiver
-sudo ./hareceiver
+ ./hareceiver
